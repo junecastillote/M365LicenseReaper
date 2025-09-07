41c3abb010ac731b7514b020f2d9f2fbbecb0772
June Castillote <june.castillote@gmail.com>
2025-09-07 15:49:26 +0800

    Create Get-UserAccountState.ps1

---
+++ source/public/Get-UserAccountState.ps1
@@ -0,0 +1,87 @@
+function Get-MLRUserAccountState {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory)]
+        [string]
+        $Username
+    )
+
+    $today = (Get-Date)
+    $todayDateString = $today.ToString('yyyy-MM-dd')
+
+    try {
+        $properties = @(
+            'UserPrincipalName',
+            'AccountEnabled'
+        )
+
+        # Get user object
+        $user = Get-MgUser -UserId $Username -ErrorAction Stop -Property $properties | Select-Object $properties
+    }
+    catch {
+        SayError "$($_.Exception.Message)"
+        if ($_.Exception.Message -like "*does not exist*") {
+            $action = 'Cancel'
+            $readinessNote = 'User account is not found.'
+        }
+        else {
+            $action = 'Skip'
+            $readinessNote = "Cannot determine readiness because there was an error whlie getting the user account. $($_.Exception.Message)"
+        }
+
+        return $([PSCustomObject]([ordered]@{
+                    Username        = $Username
+                    AccountEnabled  = ''
+                    AssignedLicense = @()
+                    Action          = $action
+                    ReadinessNote   = $readinessNote
+                }))
+    }
+
+    try {
+        # Get user licenses
+        $userLicenseCollection = @(Get-MgUserLicenseDetail -UserId $Username -ErrorAction Stop | Select-Object SkuId)
+
+        # If without license
+        if (!$userLicenseCollection) {
+            $action = 'Cancel'
+            $readinessNote = "License removal cancelled - user accout is not licensed as of $($todayDateString). This task will not be retried."
+            $assignedLicense = @()
+        }
+
+        # if with license
+        if ($userLicenseCollection) {
+            # with license + account disabled
+            if (!$user.AccountEnabled) {
+                $action = 'Remove'
+                $readinessNote = "License removal allowed - user account is disabled. This task is final."
+            }
+
+            # with license + account enabled (skip)
+            if ($user.AccountEnabled) {
+                $action = 'Skip'
+                $readinessNote = "License removal not allowed - user account is currently enabled. This task will be retried."
+            }
+
+            $assignedLicense = @($userLicenseCollection.SkuId)
+        }
+
+        return $([PSCustomObject]([ordered]@{
+                    Username        = $Username
+                    AccountEnabled  = $user.AccountEnabled
+                    AssignedLicense = $assignedLicense
+                    Action          = $action
+                    ReadinessNote   = $readinessNote
+                }))
+    }
+    catch {
+        SayError "$($_.Exception.Message)"
+        return $([PSCustomObject]([ordered]@{
+                    Username        = $Username
+                    AccountEnabled  = ''
+                    AssignedLicense = @()
+                    Action          = 'Skip'
+                    ReadinessNote   = "Cannot determine readiness because there was an error getting the user license details. $($_.Exception.Message). This task will be retried."
+                }))
+    }
+}
